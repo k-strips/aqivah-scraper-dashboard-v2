5a5e3da1133449bb0711857afa87fb73df0ca46f
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-09-03 00:06:44 +0100

    💨 Use Tailwind for styling dashboard and responsiveness

--- components/DashboardLayout.jsx
+++ components/DashboardLayout.jsx
@@ -1,59 +1,32 @@
-import {MAX_MOBILE_VIEW_WIDTH} from "config"
-import useWindowSize from "hooks/useWindowSize"
-import Link from "next/link"
-import {useState} from "react"
-import {Menu, X} from "react-feather"
-import BackButton from "./BackButton"
-import Heading from "./Header"
-import HeadingWithButton from "./HeadingWithButton"
+import { MAX_MOBILE_VIEW_WIDTH } from "config";
+import useWindowSize from "hooks/useWindowSize";
+import Link from "next/link";
+import { useState } from "react";
+import { Menu, X } from "react-feather";
+import BackButton from "./BackButton";
+import Heading from "./Header";
+import HeadingWithButton from "./HeadingWithButton";
 
-const Sidebar = ({selected = {}, isInMobileView = true}) => {
-  const [expanded, setExpanded] = useState(false)
+const Sidebar = ({ selected = {}, isInMobileView = true }) => {
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <div
-    className="bg-black"
-      style={{width: "100%", height: "100%", borderRight: "1px solid black"}}
-    >
-      {isInMobileView ? (
-        <div
-          onClick={() => setExpanded(prev => !prev)}
-          style={{
-            display: "flex",
-            width: "100%",
-            alignItems: "center",
-            justifyContent: "flex-end",
-          }}
-        >
-          {/* Click here to {expanded ? "hide" : "show"} the menu */}
-          <div
-            style={{
-              padding: "10px",
-              position: "fixed",
-              top: 0,
-              // left: 0,
-              right: 0,
-              zIndex: "100",
-            }}
-          >
-            {expanded ? <X /> : <Menu />}
-          </div>
+    <div className="w-full h-full border-r-4">
+      <div
+        onClick={() => setExpanded((prev) => !prev)}
+        className="flex w-full items-center justify-end md:hidden"
+      >
+        {/* Click here to {expanded ? "hide" : "show"} the menu */}
+        <div className="p-2 fixed top-0 right-0 z-100">
+          {expanded ? <X /> : <Menu />}
         </div>
-      ) : null}
+      </div>
+
       {expanded || !isInMobileView ? (
         <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "30px",
-            position: isInMobileView ? "absolute" : "relative",
-            background: "white",
-            // height: "80vh",
-            zIndex: "99",
-            width: "100%",
-          }}
+          className="flex flex-col p-7 absolute md:relative bg-gray-100 w-full"
           onClick={() => {
-            setExpanded(false)
+            setExpanded(false);
           }}
         >
           <Link href="/">Home</Link>
@@ -64,50 +37,33 @@ const Sidebar = ({selected = {}, isInMobileView = true}) => {
         </div>
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-const DashboardLayout = props => {
-  const {children, heading, hideBackButton} = props
-  const {width} = useWindowSize()
+const DashboardLayout = (props) => {
+  const { children, heading, hideBackButton } = props;
+  const { width } = useWindowSize();
 
   // if we're in the mobile view, place the nav bar at the top, else place it to the left
-  const isInMobileView = width < MAX_MOBILE_VIEW_WIDTH
+  const isInMobileView = width < MAX_MOBILE_VIEW_WIDTH;
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: isInMobileView ? "column" : "row",
-        minHeight: "100vh",
-        width: "100%",
-      }}
-    >
-      <div style={{width: isInMobileView ? "100vw" : "20vw"}}>
+    <div className="flex flex-col md:flex-row w-full min-h-screen">
+      <div className="w-screen md:w-20vw">
         <Sidebar isInMobileView={isInMobileView} />
       </div>
-      <div style={{padding: "10px", overflowX: "scroll"}}>
-        {!isInMobileView ? (
-          <div style={{textAlign: "right", padding: "30px 30px 0 0"}}>
-            Logout
-          </div>
-        ) : null}
 
-        <div
-          style={{
-            display: "flex",
-            width: isInMobileView ? "100vw" : "80vw",
-            alignItems: "center",
-            justifyContent: "flex-start",
-          }}
-        >
+      <div className="p-2 overflow-x-scroll">
+          <div className="text-right pt-7 pr-7 hidden md:block">Logout</div>
+
+        <div className="flex items-center justify-start w-screen md:w-80vw">
           {hideBackButton ? null : <BackButton />}
           <Heading>{heading}</Heading>
         </div>
         {children}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "80vw": "80vw",
+        "20vw": "20vw",
+      },
+      zIndex: {
+        100: "100",
+      },
+    },
   },
   variants: {
     extend: {},
